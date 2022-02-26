72bfdb34e0c9abe34e55074f8540b03bbba49e11
John Wells <molekewl2017@gmail.com>
2022-02-25 20:43:05 -0800

    Have to use babel, config webpack & babelrc file. Wholly! Async works & .env files & more!

---
+++ src/script.js
@@ -0,0 +1,21 @@
+const count = 10;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${process.env.UNSPLASH_API_KEY}&count=${count}`;
+
+console.log(process.env.UNSPLASH_API_KEY)
+
+// Get photos from Unsplash
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl)
+
+    const data = response.json();
+
+    console.log({ data });
+
+  } catch (err) {
+    console.error(error)
+  }
+}
+
+// On Load
+getPhotos();
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/script.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js?$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ]
+  },
+  devServer: {
+    static: {
+      directory: 'src',
+    },
+    hot: true,
+    port: 8000,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    new Dotenv
+  ]
+};
